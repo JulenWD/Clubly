f6ccaa5c524d75d442bff532372a0666b21b67ed
JulenAlonso123 <julen.alonsorodero@cieep.com>
2025-05-29 18:25:45 +0200

    he hecho un pequeño cambio para que la redirección de compra exitosa funcione bien

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: [
+      'https://clubly-p2t0087v8-julens-projects-2e33d71b.vercel.app', // tu frontend en Vercel
+      'http://localhost:5173' // para desarrollo local, si quieres
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
